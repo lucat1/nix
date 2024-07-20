c4a05398c0b87de21ebdb4b11e6463c975e60a83
Luca <luca@teapot.ovh>
2024-07-20 22:15:05 +0200

    230 current  2024-07-20 22:14:54  24.05.20240717.c716603  6.6.40                          *

--- home/desktop/xdg.nix
+++ home/desktop/xdg.nix
@@ -9,22 +9,30 @@
     createDirectories = true;
   };
 
-  xdg.portal = {
+  xdg.portal = let
+    hyprland = config.wayland.windowManager.hyprland.package;
+    xdph = pkgs.xdg-desktop-portal-hyprland.override {inherit hyprland;};
+  in {
     enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-hyprland
-      xdg-desktop-portal-gtk
-    ];
-
-    config = {
-      hyprland = {
-        default = [
-          "hyprland"
-          "gtk"
-        ];
-        "org.freedesktop.impl.portal.Screenshot" = ["hyprland"];
-        "org.freedesktop.impl.portal.ScreenCast" = ["hyprland"];
-      };
-    };
+    extraPortals = [xdph];
+    configPackages = [hyprland];
   };
+  #   xdg.portal = {
+  #     enable = true;
+  #     extraPortals = with pkgs; [
+  #       xdg-desktop-portal-hyprland
+  #       xdg-desktop-portal-gtk
+  #     ];
+  #
+  #     config = {
+  #       hyprland = {
+  #         default = [
+  #           "hyprland"
+  #           "gtk"
+  #         ];
+  #         "org.freedesktop.impl.portal.Screenshot" = ["hyprland"];
+  #         "org.freedesktop.impl.portal.ScreenCast" = ["hyprland"];
+  #       };
+  #     };
+  #   };
 }
