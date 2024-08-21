86b7ab18d3fc56390116798a1a7c90db1d49aa61
Luca <luca@teapot.ovh>
2024-08-22 00:52:20 +0200

    274 current  2024-08-22 00:52:15  24.05.20240805.883180e  6.6.44                          *

--- home/desktop/sway.nix
+++ home/desktop/sway.nix
@@ -13,6 +13,16 @@
     config = {
       modifier = "Mod4";
 
+      window = {
+        hideEdgeBorders = "none";
+        border = 2;
+        titlebar = false;
+      };
+
+      gaps = {
+        smartBorders = "no_gaps";
+      };
+
       terminal = "${pkgs.foot}/bin/foot";
       menu = "${pkgs.bemenu}/bin/bemenu-run --no-exec | ${pkgs.findutils}/bin/xargs swaymsg exec --";
 
