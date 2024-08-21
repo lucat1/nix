af510c530ea6ea1f90b6f6bec02b54223fbc0598
Luca <luca@teapot.ovh>
2024-08-21 23:48:42 +0200

    267 current  2024-08-21 23:48:37  24.05.20240805.883180e  6.6.44                          *

--- home/desktop/xdg.nix
+++ home/desktop/xdg.nix
@@ -9,30 +9,30 @@
     createDirectories = true;
   };
 
-  xdg.portal = let
-    hyprland = config.wayland.windowManager.hyprland.package;
-    xdph = pkgs.xdg-desktop-portal-hyprland.override {inherit hyprland;};
-  in {
+  # xdg.portal = let
+  #   hyprland = config.wayland.windowManager.hyprland.package;
+  #   xdph = pkgs.xdg-desktop-portal-hyprland.override {inherit hyprland;};
+  # in {
+  #   enable = true;
+  #   extraPortals = [xdph];
+  #   configPackages = [hyprland];
+  # };
+  xdg.portal = {
     enable = true;
-    extraPortals = [xdph];
-    configPackages = [hyprland];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+
+    config = {
+      hyprland = {
+        default = [
+          "gtk"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.Screenshot" = ["gtk"];
+        "org.freedesktop.impl.portal.ScreenCast" = ["gtk"];
+      };
+    };
   };
-  #   xdg.portal = {
-  #     enable = true;
-  #     extraPortals = with pkgs; [
-  #       xdg-desktop-portal-hyprland
-  #       xdg-desktop-portal-gtk
-  #     ];
-  #
-  #     config = {
-  #       hyprland = {
-  #         default = [
-  #           "hyprland"
-  #           "gtk"
-  #         ];
-  #         "org.freedesktop.impl.portal.Screenshot" = ["hyprland"];
-  #         "org.freedesktop.impl.portal.ScreenCast" = ["hyprland"];
-  #       };
-  #     };
-  #   };
 }
