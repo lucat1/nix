d9f8f0c6b7a97dcf32568f3bb23d99ecd453c986
Luca <luca@teapot.ovh>
2024-08-21 16:14:19 +0200

    259 current  2024-08-21 16:14:14  24.05.20240805.883180e  6.6.44                          *

--- home/programs/foot.nix
+++ home/programs/foot.nix
@@ -22,6 +22,26 @@
         hide-when-typing = true;
         alternate-scroll-mode = true;
       };
+      colors = {
+        background = vars.colors.bg;
+        foreground = vars.colors.fg;
+        regular0 = vars.colors.bg0;
+        regular1 = vars.colors.red;
+        regular2 = vars.colors.green;
+        regular3 = vars.colors.yellow;
+        regular4 = vars.colors.blue;
+        regular5 = vars.colors.purple;
+        regular6 = vars.colors.aqua;
+        regular7 = vars.colors.gray;
+        bright0 = vars.colors.lgray;
+        bright1 = vars.colors.lred;
+        bright2 = vars.colors.lgreen;
+        bright3 = vars.colors.lyellow;
+        bright4 = vars.colors.lblue;
+        bright5 = vars.colors.lpurple;
+        bright6 = vars.colors.laqua;
+        bright7 = vars.colors.fg;
+      };
     };
   };
 }
