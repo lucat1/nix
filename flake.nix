bcc96dda516e7ab964a9b74500fd4ae4afc42990
Luca <luca@teapot.ovh>
2024-08-24 17:01:24 +0200

    300 current  2024-08-24 16:56:41  24.05.20240822.797f7dc  6.6.46                          *

--- flake.nix
+++ flake.nix
@@ -1,24 +1,69 @@
-# Do not modify! This file is generated.
 {
   description = "NixOS configuration";
-  inputs = {
-    flakegen.url = "github:jorsn/flakegen";
-    home-manager = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager/release-24.05";
-    };
-    nix-index-database = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/nix-index-database";
-    };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+
   nixConfig = {
     experimental-features = ["nix-command" "flakes"];
-    extra-substituters = ["https://nix-community.cachix.org"];
-    extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
-    substituters = ["https://cache.nixos.org"];
+    substituters = [
+      "https://cache.nixos.org"
+    ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
-  outputs = inputs: inputs.flakegen ./flake.in.nix inputs;
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    home-manager,
+    nix-index-database,
+    ...
+  }: let
+    vars = import ./vars.nix;
+  in {
+    nixosConfigurations."${vars.hostname}" = nixpkgs.lib.nixosSystem {
+      system = vars.arch;
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      modules = [
+        ./machines/laptop.nix
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.users."${vars.user}" = {
+            imports = [
+              ./home
+            ];
+          };
+          home-manager.sharedModules = [
+            nix-index-database.hmModules.nix-index
+          ];
+          home-manager.extraSpecialArgs = {
+            vars = import ./vars.nix;
+            inputs = inputs;
+          };
+        }
+      ];
+    };
+  };
 }
