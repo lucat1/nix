472f9faa47aa14d22d234eae78e3f48284ac4598
Luca <luca@teapot.ovh>
2024-08-25 02:20:01 +0200

    313 current  2024-08-25 02:19:55  24.05.20240822.797f7dc  6.6.46                          *

--- home/scripts/menu.nix
+++ home/scripts/menu.nix
@@ -17,11 +17,11 @@ pkgs.writeShellApplication {
       --scrollbar autohide -l "10 down" --fixed-height --center \
       -W .75 -M 100 -B 2 --bdr "#${vars.colors.aqua}" \
       --tb "#${vars.colors.aqua}" --fb "#${vars.colors.bg}" \
-      --nb "#${vars.colors.bg}" --hb "#${vars.colors.bg}" \
+      --nb "#${vars.colors.bg}" --hb "#${vars.colors.bg0}" --ab "#${vars.colors.bg}" \
       --sb "#${vars.colors.bg}" --scb "#${vars.colors.bg}" \
       --tf "#${vars.colors.bg}" --ff "#${vars.colors.fg}" \
       --nf "#${vars.colors.fg}" --hf "#${vars.colors.aqua}" \
-      --sf "#${vars.colors.aqua}" --scf "#${vars.colors.fg}" \
+      --sf "#${vars.colors.aqua}" --scf "#${vars.colors.purple}" \
       "${rest}"
   '';
 }
