0ad4435930991484e5b520914ea03e547bf2419e
Luca <luca@teapot.ovh>
2024-08-26 23:52:52 +0200

    322 current  2024-08-26 23:52:46  24.05.20240822.797f7dc  6.6.46                          *

--- home/desktop/hypridle.nix
+++ home/desktop/hypridle.nix
@@ -7,22 +7,25 @@
 }: {
   services.hypridle = {
     enable = true;
-    settings = {
+    settings = let
+      hyprlock = "${pkgs.hyprlock}/bin/hyprlock";
+      swaymsg = "${pkgs.sway}/bin/swaymsg";
+    in {
       general = {
-        after_sleep_cmd = "swaymsg output * dpms on";
+        after_sleep_cmd = "${swaymsg} output * dpms on";
         ignore_dbus_inhibit = false;
-        lock_cmd = "hyprlock";
+        lock_cmd = hyprlock;
       };
 
       listener = [
         {
           timeout = 10 * 60; # 10 minutes
-          on-timeout = "hyprlock";
+          on-timeout = hyprlock;
         }
         {
           timeout = 15 * 60; # 15 minutes
-          on-timeout = "swaymsg output * dpms off";
-          on-resume = "swaymsg output * dpms on";
+          on-timeout = "${swaymsg} output * dpms off";
+          on-resume = "${swaymsg} output * dpms on";
         }
       ];
     };
