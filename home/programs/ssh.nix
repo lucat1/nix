1759bcb93e0a5b53da8cf7a13ff4edd73bb5885e
Luca <luca@teapot.ovh>
2024-09-03 22:18:33 +0200

    336 current  2024-09-03 22:18:15  24.05.20240822.797f7dc  6.6.46                          *

--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -7,7 +7,13 @@
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
-    matchBlocks = {
+    matchBlocks = let
+      proxmoxForward = {
+        bind.port = 8006;
+        host.address = "127.0.0.1";
+        host.port = 8006;
+      };
+    in {
       "*" = {
         setEnv = {
           TERM = "xterm-256color";
@@ -17,25 +23,19 @@
         hostname = "honey.teapot.ovh";
         port = 2222;
         user = "root";
-        localForwards = [
-          {
-            bind.port = 8006;
-            host.address = "127.0.0.1";
-            host.port = 8006;
-          }
-        ];
+        localForwards = [proxmoxForward];
+      };
+      "picione" = {
+        hostname = "10.1.1.125";
+        port = 22;
+        user = "root";
+        localForwards = [proxmoxForward];
       };
       "kepler" = {
         hostname = "kepler.teapot.ovh";
         port = 2222;
         user = "root";
-        localForwards = [
-          {
-            bind.port = 8006;
-            host.address = "127.0.0.1";
-            host.port = 8006;
-          }
-        ];
+        localForwards = [proxmoxForward];
       };
     };
   };
