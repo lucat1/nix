f6b40bdd24d4255dd9ddd09d4f0feb37a9c45dd2
Luca <luca@teapot.ovh>
2024-12-27 15:52:49 +0100

    386 current  2024-12-27 15:52:47  24.05.20241222.6df9247  6.6.67                          *

--- configuration.nix
+++ configuration.nix
@@ -36,10 +36,10 @@ in {
   };
   services.resolved = {
     enable = true;
-    dnssec = "false"; # "allow-downgrade";
+    dnssec = "allow-downgrade"; # "false";
     domains = ["~."];
     fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
-    dnsovertls = "false"; # "opportunistic";
+    dnsovertls = "opportunistic"; # "false";
   };
   systemd.services.nssd = {
     enable = false;
