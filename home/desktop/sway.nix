af0d67fa2740655df485821fa9f5d14cab7bd31e
Luca <luca@teapot.ovh>
2025-10-01 22:38:40 +0200

    465 current  2025-10-01 22:38:35  25.05.20250819.a58390a  6.12.42                          *

--- home/desktop/sway.nix
+++ home/desktop/sway.nix
@@ -12,7 +12,7 @@
       xkb_types     { include "complete" };
       xkb_compat    { include "complete" };
       xkb_symbols   {
-        include "pc+us+us(intl)+inet(evdev)+altwin(swap_alt_win)+capslock(escape)+group(shifts_toggle)"
+        include "pc+us+us(intl):2+inet(evdev)+altwin(swap_alt_win)+capslock(escape)+group(shifts_toggle)"
 
         // Swap grave/tilde with less/greater
         replace key <TLDE> { [ less, greater, less, greater ] };
