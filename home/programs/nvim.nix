9a30a375b271750622bd69579d797aae602616ab
Luca <luca@teapot.ovh>
2025-10-09 15:59:10 +0200

    490 current  2025-10-09 15:59:04  25.05.20251001.5b5be50  6.12.49                          *

--- home/programs/nvim.nix
+++ home/programs/nvim.nix
@@ -6,14 +6,14 @@
   inputs,
   ...
 }: {
-  home.file = {
-    ".local/share/nvim/site/pack/paks/start/visimp".source = pkgs.fetchFromGitHub {
-      owner = "visimp";
-      repo = "visimp";
-      rev = "d1b7a2509831fc08bbcd3e296c69f92e7efa8247"; # latest commit on master as of 09/10/2025
-      sha256 = "sha256-ZPNqFJS66xYztPu/SO1PGNKiWMlNoJQXtFy2f4cheZ8="; # lib.fakeSha256;
-    };
-  };
+  # home.file = {
+  #   ".local/share/nvim/site/pack/paks/start/visimp".source = pkgs.fetchFromGitHub {
+  #     owner = "visimp";
+  #     repo = "visimp";
+  #     rev = "d1b7a2509831fc08bbcd3e296c69f92e7efa8247"; # latest commit on master as of 09/10/2025
+  #     sha256 = "sha256-ZPNqFJS66xYztPu/SO1PGNKiWMlNoJQXtFy2f4cheZ8="; # lib.fakeSha256;
+  #   };
+  # };
 
   programs.neovim = {
     enable = true;
